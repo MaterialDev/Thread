115f95b85f99656dcd2b204f1b494890e96573da
Travis-CI <travis@material.com>
2016-09-23 10:05:30 -0500

    Add keyboard functionality

--- components/select/select.directive.ts
+++ components/select/select.directive.ts
@@ -1,17 +1,29 @@
+import SelectController = Thread.Components.SelectController;
 module Thread.Components {
+    export class OptionModel {
+        name: String;
+        value: any;
+        isHighlighted: boolean;
+
+        constructor(name: String, value: any) {
+            this.name = name;
+            this.value = value;
+            this.isHighlighted = false;
+        }
+    }
+
     export class SelectController {
         options = [];
-        selected = 'Test 1';
+        selected: OptionModel;
+        model: any;
+        isOpen: boolean;
 
         constructor(private $element: ng.IAugmentedJQuery) {
 
         }
 
         addOption(name, value) {
-            this.options.push({
-                name: name,
-                value: value
-            });
+            this.options.push(new OptionModel(name, value));
         }
 
         openOptionList() {
@@ -26,6 +38,12 @@ module Thread.Components {
             optionList.style.top = `${parentPos.top - 14}px`;
             angular.element(optionList).addClass('is-open');
             angular.element(backdrop).addClass('is-open');
+
+            if (this.selected) {
+                this.selected.isHighlighted = true;
+            }
+
+            this.isOpen = true;
         }
 
         closeOptionList() {
@@ -33,11 +51,73 @@ module Thread.Components {
             let backdrop: HTMLElement = <HTMLElement>this.$element[0].querySelector('.js-select__backdrop');
             angular.element(optionList).removeClass('is-open');
             angular.element(backdrop).removeClass('is-open');
+
+            this.isOpen = false;
         }
 
         select(option) {
-            (<any>this).model = option.value;
+            this.selected = option;
+            this.model = option.value;
             this.closeOptionList();
+        }
+
+        highlightNext() {
+            let idx: number = -1;
+
+            for (let i = 0; i < this.options.length; i++) {
+                if (this.options[i].isHighlighted) {
+                    idx = i;
+                    this.options[i].isHighlighted = false;
+                    break;
+                }
+            }
+
+            if (idx >= this.options.length - 1 || idx == -1) {
+                idx = 0;
+            } else {
+                idx += 1;
+            }
+
+            this.unHighlightAll();
+            this.selected = this.options[idx];
+            this.options[idx].isHighlighted = true;
+        }
+
+        highlightPrev() {
+            let idx: number = -1;
+
+            for (let i = 0; i < this.options.length; i++) {
+                if (this.options[i].isHighlighted) {
+                    idx = i;
+                    this.options[i].isHighlighted = false;
+                    break;
+                }
+            }
+
+            if (idx <= 0) {
+                idx = this.options.length - 1;
+            } else {
+                idx -= 1;
+            }
+
+            this.selected = this.options[idx];
+            this.options[idx].isHighlighted = true;
+        }
+
+        getHighlighted() {
+            for (let i = 0; i < this.options.length; i++) {
+                if (this.options[i].isHighlighted) {
+                    return this.options[i];
+                }
+            }
+        }
+
+        unHighlightAll() {
+            for (let option of this.options) {
+                option.isHighlighted = false;
+            }
+
+            this.selected = null;
         }
     }
 
@@ -46,34 +126,70 @@ module Thread.Components {
     }
 }
 
-angular.module('thread.select', []).directive('tdSelect', () => {
+angular.module('thread.select', []).directive('tdSelect', ($timeout: ng.ITimeoutService) => {
     return {
         scope: {
             model: '=ngModel'
         },
-        template: `<div class="c-select c-input__field" tabindex="0" ng-click="$selectCtrl.openOptionList();">
+        template: `<div class="c-select c-input__field js-select" tabindex="0" ng-click="$selectCtrl.openOptionList();">
                         <div class="c-select__backdrop js-select__backdrop"></div>
                         <span aria-hidden="true" class="c-select__value">{{$selectCtrl.model || ' '}}</span>
                         <ul aria-hidden="true" class="c-select__menu js-select__menu">
-                            <li class="c-select__menu-item" ng-repeat="option in $selectCtrl.options"
-                                ng-click="$selectCtrl.select(option); $event.stopPropagation()">{{option.name}}
+                            <li class="c-select__menu-item js-select__menu-item" ng-repeat="option in $selectCtrl.options" ng-class="{'has-focus': option.isHighlighted}"
+                                ng-click="$selectCtrl.select(option); $event.stopPropagation()">{{option.name}} {{option.isHighlighted}}
                             </li>
                         </ul>
                         <i class="mi c-select__arrow" aria-hidden="true">arrow_drop_down</i>
-                        <select class="c-select__box" ng-transclude ng-model="$selectCtrl.model"></select>
+                        <select class="c-select__box" ng-transclude ng-model="$selectCtrl.model" tabindex="-1"></select>
                     </div>`,
         controller: Thread.Components.SelectController,
         bindToController: true,
         controllerAs: '$selectCtrl',
         transclude: true,
         replace: true,
-        link(scope, element, attrs, ctrl) {
+        link(scope: ng.IScope, element: ng.IAugmentedJQuery, attrs: any, ctrl: SelectController) {
             let backdrop = angular.element(element[0].querySelector('.js-select__backdrop'));
+
+            $timeout(() => {
+                let option = angular.element(element[0].querySelectorAll('.js-select__menu-item'));
+
+                option.on('mouseenter mouseleave', () => {
+                    ctrl.unHighlightAll();
+                    scope.$apply();
+                });
+            });
 
             backdrop.on('click', (e) => {
                 e.stopPropagation();
                 ctrl.closeOptionList();
             });
+
+            element.on('keyup', (e) => {
+                switch (e.which) {
+                    case 38:    //arrow up
+                    case 37:    //arrow left
+                        if (!ctrl.isOpen) ctrl.openOptionList();
+                        ctrl.highlightPrev();
+                        scope.$apply();
+                        break;
+                    case 39:    //arrow right
+                    case 40:    //arrow down
+                        if (!ctrl.isOpen) ctrl.openOptionList();
+                        ctrl.highlightNext();
+                        scope.$apply();
+                        break;
+                    case 32:    //space
+                        ctrl.isOpen ? ctrl.closeOptionList() : ctrl.openOptionList();
+                        scope.$apply();
+                        break;
+                    case 13:    //enter
+                        if (ctrl.isOpen && ctrl.selected) {
+                            ctrl.select(ctrl.selected);
+                            scope.$apply();
+                        }
+                        break;
+                }
+            })
         }
     };
 });
